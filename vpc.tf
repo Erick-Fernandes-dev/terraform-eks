12b47aef834ed51a053b3fcc5f7ed3bb2ac216f3
erick_fernandes <ericktbwa2000@gmail.com>
2025-01-08 17:20:30 -0300

    chore: Subnets and Variables in Terraform

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "eks_vpc" {
   # Habilitando DNS Support e DNS Hostnames
   enable_dns_support   = true
   enable_dns_hostnames = true
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.cidr_block
 
 
   # Implementando tags
