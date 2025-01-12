a21dfcc61acb59e8ff844d9c027cf48fa3f14328
erick_fernandes <ericktbwa2000@gmail.com>
2025-01-12 07:00:30 -0300

    feat: Private route table

---
+++ ngw.tf
@@ -0,0 +1,87 @@
+resource "aws_eip" "eks_ngw_eip_1a" {
+
+  # No curso foi usado o 'vpc = true' para criar um Elastic IP na VPC
+  # Porém, ele está depreciado e não e está sendo mais usado
+
+  # O correto é usar o 'domain = "vpc"' para criar um Elastic IP na VPC
+  domain = "vpc"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip-1a"
+    }
+  )
+}
+
+resource "aws_eip" "eks_ngw_eip_1b" {
+
+  # No curso foi usado o 'vpc = true' para criar um Elastic IP na VPC
+  # Porém, ele está depreciado e não e está sendo mais usado
+
+  # O correto é usar o 'domain = "vpc"' para criar um Elastic IP na VPC
+  domain = "vpc"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip-1b"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "eks_ngw_1a" {
+  allocation_id = aws_eip.eks_ngw_eip_1a.id
+  subnet_id     = aws_subnet.eks_subnet_public_1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1a"
+    }
+  )
+
+}
+
+resource "aws_nat_gateway" "eks_ngw_1b" {
+  allocation_id = aws_eip.eks_ngw_eip_1b.id
+  subnet_id     = aws_subnet.eks_subnet_public_1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1b"
+    }
+  )
+
+}
+
+resource "aws_route_table" "eks_private_route_table_1a" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1a.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-priv-route-table-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1b" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-priv-route-table-1b"
+    }
+  )
+}
