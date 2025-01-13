fd91d270b12fcb7f5ff13d52fa698b65a24dda91
erick_fernandes <ericktbwa2000@gmail.com>
2025-01-13 10:39:37 -0300

    feat: Implement cluster mudele

--- modules.tf
+++ modules.tf
@@ -5,3 +5,9 @@ module "eks_network" {
   cidr_block   = var.cidr_block
   tags         = local.tags
 }
+
+module "eks_cluster" {
+  source       = "./modules/cluster"
+  project_name = var.project_name
+  tags         = local.tags
+}
---
+++ modules/cluster/iam.tf
@@ -0,0 +1,36 @@
+# Arquivo responsável por criar as permissões necessárias para o EKS gerenciar clusters
+
+# Vamos criar uma role para o EKS gerenciar clusters
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.project_name}-cluster-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-cluster-role"
+    }
+  )
+}
+
+# Vamos anexar a policy AmazonEKSClusterPolicy ao role criado que permite o EKS gerenciar clusters
+resource "aws_iam_policy_attachment" "eks_cluster_role_attachment" {
+  name       = "${var.project_name}-cluster-role-attachment"
+  roles      = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
