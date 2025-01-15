f001a09fcda904212b1f5e496658ddd0a29394a9
erick_fernandes <ericktbwa2000@gmail.com>
2025-01-15 05:07:39 -0300

    feat: Cluster module

--- output.tf
+++ output.tf
@@ -3,3 +3,14 @@ output "subnet_pub_1a" {
   value = module.eks_network.subnet_pub_1a
 
 }
+
+# Fazendo a chama do output do módulo cluster imprimir a configuração da VPC
+output "eks_vpc_config" {
+  value = module.eks_cluster.eks_vpc_config
+
+}
+
+output "oidc" {
+  value = module.eks_cluster.oidc
+
+}
